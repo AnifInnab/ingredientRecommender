6be53bd42d569f253a3863bd5ea7e91a5bdc7989
Orvar Korvarsson <Korvlax@Orvars-MacBook-Air.local>
2019-11-04 13:34:33 +0100

    refactored

--- src/recipe_embeddings.py
+++ src/recipe_embeddings.py
@@ -2,91 +2,104 @@ import pandas as pd
 import numpy as np
 import ast
 from gensim.models import Word2Vec
-from sklearn.manifold import TSNE
+#from sklearn.manifold import TSNE
+from MulticoreTSNE import MulticoreTSNE as TSNE
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from sklearn.preprocessing import normalize
+from sklearn.preprocessing import MinMaxScaler
 import pickle
 import sys
 import argparse
 
 #HELPERS
-def filter_recipes(recipes, ingredients_set):
-    """
-    removes recipe if ingredients in recipe is not in our ingredients list
-    """
-    filtered_recipes = []
-    recipes_ids = []
-    for i, recipe in enumerate(recipes):
-        found = True
-        for word in recipe:
-            if word not in ingredients_set:
-                found = False
-                break
-        if found and len(recipe) > 0:
-            filtered_recipes.append(recipe)
-            recipes_ids.append(i)
-    return filtered_recipes, recipes_ids
-
-def embed_recipe(recipe, model):
-    recipe_embedding = np.empty([len(recipe), model.vector_size])
-    for i, ingredient in enumerate(recipe):
-        recipe_embedding[i] = model.wv[ingredient]
-    
-    return normalize(recipe_embedding)
-
-def add_recipe_embedding(recipe_embedding, model):
-    resultant = np.zeros(model.vector_size)
-    for ingr_embd in recipe_embedding:
-        resultant += ingr_embd
-    return resultant
-
 def read_data(path):
-    recipes = pd.read_csv(path)
-    recipes_df = recipes['ingredients']
-    recipes_df = recipes_df.apply(ast.literal_eval)
-    recipes = list(recipes_df)
-    return recipes, recipes_df
+    df = pd.read_csv(path)
+    df['ingredients'] = df['ingredients'].apply(ast.literal_eval)
+    
+    PP_df = pd.read_csv('../data/PP_recipes.csv')
+    PP_df = PP_df[['id','techniques']]
 
-def create_train_model(recipes, min_count=800):
+    #use only recipes from the PP_recipes df, also take the techniques column from it. Dont use their tokenization though...
+    df_merged = pd.merge(df, PP_df, left_on='id', right_on='id', how='right')
+    return df_merged
+
+def create_model(min_count=1000):
     model = Word2Vec(min_count=min_count,
                         window=3,
                         size=50,
                         sg=0
                         )
+    return model
+
+def filter_recipes(recipes, vocab_set):
+    """
+    removes recipe if ingredients in recipe is not in our ingredients list
+    """
+    recipes_ids = []
+    for i, recipe in enumerate(recipes):
+        found = True
+        for word in recipe:
+            if word not in vocab_set:
+                found = False
+                break
+        if found and len(recipe) > 0:
+            recipes_ids.append(i)
+    return recipes_ids
+
+def create_vocab(model, recipes):
     model.build_vocab(recipes, progress_per=100)
     vocab = list(model.wv.vocab)
     vocab_set = set(vocab)
+    return vocab, vocab_set
 
-    #Find new recipes which only has ingredients present in vocab
-    filtered_recipes, recipes_ids = filter_recipes(recipes, vocab_set)
+def train_model(model, recipes):
+    model.train(recipes, total_examples=model.corpus_count, epochs=30, report_delay=1)
 
-    model.train(filtered_recipes, total_examples=model.corpus_count, epochs=30, report_delay=1)
+def get_recipe_embeddings(model, recipes):
 
-    return model, filtered_recipes, recipes_ids
+    def embed_recipe(recipe, model):
+        recipe_embedding = np.empty([len(recipe), model.vector_size])
+        for i, ingredient in enumerate(recipe):
+            recipe_embedding[i] = model.wv[ingredient]
+        return recipe_embedding
 
-def get_recipe_embeddings(filtered_recipes, model):
+    def add_recipe_embedding(recipe_embedding, model):
+        resultant = np.zeros(model.vector_size)
+        for ingr_embd in recipe_embedding:
+            resultant += ingr_embd
+        return resultant
+
     #embed all ingredients in each recipe
-    recipe_embeddings = list(map(lambda x: embed_recipe(x, model), tqdm(filtered_recipes)))
+    recipe_embeddings = list(map(lambda x : embed_recipe(x, model), tqdm(recipes)))
 
     #add all ingredients eembedding in each recipe
     added_recipe_embeddings = list(map(lambda x : add_recipe_embedding(x, model), tqdm(recipe_embeddings)))
     added_recipe_embeddings = np.array(added_recipe_embeddings)
 
+    #scaling
+    # scaler = MinMaxScaler()
+    # scaler.fit(added_recipe_embeddings)
+    # scaled_embd = scaler.transform(added_recipe_embeddings)
+
     return added_recipe_embeddings
+    
+    
 
 def tsne(X):
     #TSNE on recipe embeddings
-    tsne = TSNE(n_components=2, verbose=1)
+    tsne = TSNE(n_components=2, verbose=1, n_jobs=2)
     X_tsne = tsne.fit_transform(X)
+    
     return X_tsne
 
-def hover_plot(X_tsne, names):
+def hover_plot(X_tsne, df):
 
     def update_annot(ind):
         pos = sc.get_offsets()[ind["ind"][0]]
         annot.xy = pos
-        text = names[ind['ind'][0]]
+        hover_idx = ind['ind'][0]
+        text = "{} \n {}".format(df['name'].iloc[hover_idx], df['ingredients'].iloc[hover_idx])
         annot.set_text(text)
         annot.get_bbox_patch().set_alpha(0.4)
 
@@ -132,27 +145,31 @@ def load_pkl(path):
     print('Loaded ', path)
     return pickle.load(pickle_in)
 
-
-class Step:
-    def __init__(self, load, path):
-        self.load = load
-        self.path = path
-
 def main(args):
     pkl_path = args.pkl_path
     data_path = args.data_path
     load = args.load
+
     if load:
-        recipes, recipes_df, model, filtered_recipes, recipes_ids, X, X_tsne = load_pkl(pkl_path)
+        df, df_filtered, model, recipes_ids, X, X_tsne, vocab, vocab_set = load_pkl(pkl_path)
     else:
-        recipes, recipes_df = read_data(data_path)
-        model, filtered_recipes, recipes_ids = create_train_model(recipes)
-        X = get_recipe_embeddings(filtered_recipes, model)
+        df = read_data(data_path)
+        recipes = df['ingredients']
+
+        model = create_model(min_count=2000)
+        vocab, vocab_set = create_vocab(model, recipes)
+        recipes_keep_ids = filter_recipes(recipes, vocab_set)
+
+        df_filtered = df.iloc[recipes_keep_ids]
+        filtered_recipes = df_filtered['ingredients']
+        train_model(model, filtered_recipes)
+
+        X = get_recipe_embeddings(model, filtered_recipes)
+
         X_tsne = tsne(X)
-        save_pkl((recipes,recipes_df, model, filtered_recipes, recipes_ids, X, X_tsne), pkl_path)
+        save_pkl((df, df_filtered, model, recipes_keep_ids, X, X_tsne, vocab, vocab_set), pkl_path)
         
-    names = list(recipes_df[recipes_ids])
-    hover_plot(X_tsne, names)
+    hover_plot(X_tsne, df_filtered)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
